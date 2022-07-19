9f41c5cebeaa12e574dc251c4f917b79b7f8161f
Tiberio Ferreira <tiberio.ferreiracs@gmail.com>
2022-07-19 18:40:23 -0300

    exercicio a007-variaveis

---
+++ exercicio2/script.js
@@ -0,0 +1,15 @@
+const perguntaA = "Você comeu hoje?";
+let respostaA = prompt (perguntaA);
+const perguntaB = "Você tomou água?";
+let respostaB = prompt (perguntaB);
+const perguntaC = "Você viu o sol hoje?";
+let respostaC = prompt (perguntaC);
+
+
+let sim = "sim";
+let nao = "não";
+let naoSei = "não sei";
+
+console.log(perguntaA, "-", sim);
+console.log(perguntaB, "-", sim);
+console.log(perguntaC, "-", naoSei);
---
+++ exercicio3/script.js
@@ -0,0 +1,9 @@
+let a = 10
+let b = 25
+let c;
+a=b
+c=a
+a=c
+
+console.log("o novo valor de a", b);
+console.log("o novo valor de b", a);
