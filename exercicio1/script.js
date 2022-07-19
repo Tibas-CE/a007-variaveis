2590f67cc12600e0fd80132e061391e555144418
Tiberio Ferreira <tiberio.ferreiracs@gmail.com>
2022-07-19 20:58:54 -0300

    correcoes

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,13 +1,20 @@
-const nome = prompt ("Qual seu nome?")
-let idade = prompt ("Qual sua idade?")
-console.log(nome);
-console.log(idade);
+let nome; 
+let idade; 
+
 console.log(typeof nome);
 console.log(typeof idade);  
 
-/*percebemos que as duas variáveis são em formato de strings,
-mesmo a idade sendo um número, pois a idade e adiquirida
-através de uma pergunta*/
+//a variável ficou undefined pois elas não possuem valor
 
+nome = prompt("Qual seu nome?");
+console.log(nome);
+idade = prompt("Qual sua idade?");
+//também podemos usar idade = Number (prompt"qual sua idade?")
+console.log(idade);
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+//string, pois o prompt sempre retorna uma string
 
 console.log("Olá", nome,  "você tem", idade, "anos");
